f32383c6a1c28f64dab86872d1551aa9ae57f409
Charlie DiFranco <charlie.difranco2@gmail.com>
2020-08-12 19:43:26 -0500

    finished social

--- projects/ancestor/ancestor.py
+++ projects/ancestor/ancestor.py
@@ -1,3 +1,68 @@
 
+
+# Plan
+'''
+1.  Build graph with the ancestor and (parent, child) pairs
+2.  Perform DFS on given starting node
+'''
+
 def earliest_ancestor(ancestors, starting_node):
-    pass
+
+    #build graph
+    vertices = {}
+
+    for pair in ancestors:
+        v = pair[1]
+
+        if v not in vertices:
+            vertices[v] = set()
+
+        vertices[v].add(pair[0])
+    
+    paths = []
+
+    #search graph depth-first
+    def dfs_search(path, visited=set()):
+        current_v = path[-1]
+        
+        if current_v not in vertices:
+            paths.append(path)
+        else:
+            children = vertices[current_v]  
+            for v in children:
+                if v not in visited:
+                    visited.add(v)
+                    new_path = list(path)
+                    new_path.append(v)
+                    dfs_search(new_path, visited)
+
+    dfs_search([starting_node])
+
+    
+    max_len_index = 0
+    for i in range(1, len(paths)):
+        if len(paths[i]) > len(paths[max_len_index]):
+            max_len_index = i
+        elif len(paths[i]) == len(paths[max_len_index]):
+            if paths[i][-1] < paths[max_len_index][-1]:
+                max_len_index = i
+
+    if len(paths[max_len_index]) > 1:
+        return paths[max_len_index][-1]
+    
+    return -1
+        
+
+
+
+
+
+
+
+
+
+test_ancestors = [(1, 3), (2, 3), (3, 6), (5, 6), (5, 7), (4, 5), (4, 8), (8, 9), (11, 8), (10, 1)]
+print(earliest_ancestor(test_ancestors, 1)) #10    
+print(earliest_ancestor(test_ancestors, 2)) #-1
+print(earliest_ancestor(test_ancestors, 3)) #10
+
--- projects/social/social.py
+++ projects/social/social.py
@@ -1,3 +1,18 @@
+import random
+
+class Queue():
+    def __init__(self):
+        self.queue = []
+    def enqueue(self, value):
+        self.queue.append(value)
+    def dequeue(self):
+        if self.size() > 0:
+            return self.queue.pop(0)
+        else:
+            return None
+    def size(self):
+        return len(self.queue)
+
 class User:
     def __init__(self, name):
         self.name = name
@@ -42,11 +57,27 @@ class SocialGraph:
         self.last_id = 0
         self.users = {}
         self.friendships = {}
-        # !!!! IMPLEMENT ME
 
+        # !!!! IMPLEMENT ME
+        total_friendships = avg_friendships * num_users // 2
+        friendship_combos = []
+        
         # Add users
+        for i in range(num_users):
+            self.add_user(i + 1)     
 
         # Create friendships
+        for i in range(1, num_users + 1):
+            for j in range(i + 1, num_users + 1):
+                friendship_combos.append((i, j))
+        
+        random.shuffle(friendship_combos)
+
+        for i in range(total_friendships):
+            friendship = friendship_combos[i]
+            user_id = friendship[0]
+            friend_id = friendship[1]
+            self.add_friendship(user_id, friend_id)
 
     def get_all_social_paths(self, user_id):
         """
@@ -57,9 +88,35 @@ class SocialGraph:
 
         The key is the friend's ID and the value is the path.
         """
-        visited = {}  # Note that this is a dictionary, not a set
-        # !!!! IMPLEMENT ME
-        return visited
+        connections = {}  # Note that this is a dictionary, not a set
+
+        # !!!! IMPLEMENT ME]
+        for v in self.friendships:
+            path = self.bfs(user_id, v)
+            if path is not None:
+                connections[v] = path
+
+        return connections
+    
+    def bfs(self, user_id, target):
+        visited = set()
+
+        q = Queue()
+        q.enqueue([user_id])
+
+        while q.size() > 0:
+            path = q.dequeue()
+            node = path[-1]
+            visited.add(node)
+
+            if node == target:
+                return path
+
+            for v in self.friendships[node]:
+                if v not in visited:
+                    path_copy = list(path)
+                    path_copy.append(v)
+                    q.enqueue(path_copy)
 
 
 if __name__ == '__main__':
